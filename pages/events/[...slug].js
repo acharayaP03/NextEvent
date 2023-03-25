95e53f8058dc17e59cd2d9e523c457afac609617
acharayaP03 <acharyaP03@gmail.com>
2023-03-26 09:42:23 +1100

    Display all event associated to filter query

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,11 @@
 
 import { useRouter } from "next/router";
 import {getFilteredEvents} from "../../data";
+import EventList from "../../components/events/EventList";
+import {Fragment} from "react";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/UI/button";
+import ErrorAlert from "../../components/UI/error-alert";
 
 export default function FilteredEventPage() {
 	const router = useRouter();
@@ -15,7 +20,10 @@ export default function FilteredEventPage() {
 	const filteredYear = Number(slug[0]);
 
 	if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredYear < 2021 || filteredMonth > 12  ){
-		return <p>Invalid filter, please adjust your filter and proceed again.</p>
+		return <Fragment>
+			<ErrorAlert>Invalid filter, please adjust your filter and proceed again.</ErrorAlert>
+			<Button link='/events' className='center'>Show All Events</Button>
+		</Fragment>
 	}
 
 	const filteredEvents = getFilteredEvents({
@@ -25,15 +33,19 @@ export default function FilteredEventPage() {
 
 	// check if the events are within the range
 	if(!filteredEvents || filteredEvents.length === 0){
-		return <p>No events found for the chosen filter!</p>
+		return <Fragment>
+			<ErrorAlert>No events found for the chosen filter!</ErrorAlert>
+			<div className='center'>
+				<Button link='/events' className='center'>Show All Events</Button>
+			</div>
+		</Fragment>
 	}
 
-
-
-	console.log(filteredYear,filteredMonth)
+	const date = new Date(filteredYear, filteredMonth)
 	return (
-		<div>
-			<h1>FilteredEventPage</h1>
-		</div>
+		<Fragment>
+			<ResultsTitle date={date}/>
+			<EventList items={filteredEvents}/>
+		</Fragment>
 	);
 }
