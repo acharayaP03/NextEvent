84b0bcf94080e9f6a63671e881fd48fabbd05ca3
acharayaP03 <acharyaP03@gmail.com>
2023-03-25 22:41:02 +1100

    On search props

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,20 +1,32 @@
 import Button from "../UI/button";
 import classes from "./events-search.module.css"
-export default function EventsSearch(){
+import {useRef} from "react";
+export default function EventsSearch(props){
     const monthArr = ['January', 'February', "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const yearInputRef = useRef();
+    const monthInputRef = useRef();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const selectedYear = yearInputRef.current.value;
+        const selectedMonth = monthInputRef.current.value;
+
+        props.onSearch(selectedYear, selectedMonth);
+    }
+
     return(
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.controls}>
                 <div className={classes.control}>
                     <label htmlFor="year">Year</label>
-                    <select id="year">
+                    <select id="year" ref={yearInputRef}>
                         <option value="2021">2021</option>
                         <option value="2022">2022</option>
                     </select>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="month">Month</label>
-                    <select id="month">
+                    <select id="month" ref={monthInputRef}>
 
                         { monthArr.map((month, index) => (
                             <option value={index + 1 } key={month}>{month}</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -3,12 +3,20 @@ import {getAllEvents} from "../../data";
 import EventList from "../../components/events/EventList";
 import {Fragment} from "react";
 import EventsSearch from "../../components/events/events-search";
+import {useRouter} from "next/router";
 export default function AllEvents() {
 	const items = getAllEvents();
+	const router = useRouter();
+
+	const findEventsHandler = (year, month) =>{
+		const fullPath = `/events/${year}/${month}`
+
+		router.push(fullPath)
+	}
 	return (
 		<div>
 			<Fragment>
-				<EventsSearch/>
+				<EventsSearch onSearch={findEventsHandler}/>
 				<EventList items={items}/>
 			</Fragment>
 		</div>
