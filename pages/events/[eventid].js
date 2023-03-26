9507839de9cae6e3abb3d6f0b941e83dc26cdb8e
acharayaP03 <acharyaP03@gmail.com>
2023-03-26 19:58:11 +1100

    generating pages in the server with dynamic id

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,15 +1,12 @@
-import { useRouter } from "next/router";
-import {getEventById} from "../../data";
+
+import { getAllEvents, getEventById} from "../../helpers/api-utils";
 
 import { EventContent, EventLogistics, EventSummary } from '../../components/event-detail'
 
 import {ErrorAlert} from "../../components/UI";
 
-export default function EventDetailPage() {
-
-	const router = useRouter();
-	const { eventid } = router.query;
-	const eventDetailsWithId = getEventById(eventid);
+export default function EventDetailPage(props) {
+	const eventDetailsWithId = props.selectedEventWithId;
 
 	if(!eventDetailsWithId){
 		return <ErrorAlert>Sorry, couldn't find any event associated with that event id. </ErrorAlert>
@@ -24,4 +21,26 @@ export default function EventDetailPage() {
 			</EventContent>
 		</>
 	);
+}
+
+export async function getStaticPaths(){
+	const events = await getAllEvents();
+
+	const paths = events.map( event => ({ params : { eventid: event.id }}));
+	return {
+		paths,
+		fallback: false
+	}
+}
+
+export async function getStaticProps (context) {
+	const eventId = context.params.eventid;
+
+	const selectedEventWithId = await getEventById(eventId)
+
+	return{
+		props:{
+			selectedEventWithId
+		}
+	}
 }
