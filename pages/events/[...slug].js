72182983833e88dd2ea82ad3e48619b30d0d1ed9
acharayaP03 <acharyaP03@gmail.com>
2023-03-27 19:37:33 +1100

    client side rendering

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,32 +1,58 @@
-
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import {getFilteredEvents} from "../../helpers/api-utils";
-
+import useSWR from 'swr'
 
 import { EventList, ResultsTitle} from '../../components/events'
 import { CustomButton, ErrorAlert } from '../../components/UI'
 
 
-export default function FilteredEventPage(props) {
-	// const router = useRouter();
-	// const { slug } = router.query;
+export default function FilteredEventPage() {
+	const [loadedEvents, setLoadedEvents] = useState([]);
 
-	// // slug is filter query that is passed from either event page or main page eg: ["2011", "2"]
-	// if(!slug){
-	// 	return <p className="center">Loading data...., please wait.</p>
-	// }
-	//
-	// const filteredMonth = Number(slug[1])
-	// const filteredYear = Number(slug[0]);
+	const router = useRouter();
+	const { slug } = router.query;
+	console.log(slug)
 
-	if(props.hasError){
+	const fetcher = (...args) => fetch(...args).then(res => res.json())
+	const { data, error, isLoading } = useSWR('https://next-project-2dfa5-default-rtdb.firebaseio.com/events.json', fetcher);
+
+	useEffect(() => {
+		if(data){
+			const events = [];
+
+			//convert data comming from firebase to arrays.
+			for( const key in data){
+				events.push({
+					id: key,
+					...data[key]
+				})
+			}
+			setLoadedEvents(events)
+		}
+	}, [data])
+
+	// slug is filter query that is passed from either event page or main page eg: ["2011", "2"]
+	if(!loadedEvents){
+		return <p className="center">Loading data...., please wait.</p>
+	}
+
+	const filteredYear = Number(slug[0]);
+	const filteredMonth = Number(slug[1])
+
+	if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredYear < 2021 || filteredMonth > 12 || error){
 		return <>
 			<ErrorAlert>Invalid filter, please adjust your filter and proceed again.</ErrorAlert>
 			<CustomButton link='/events' className='center'>Show All Events</CustomButton>
 		</>
 	}
 
-	const filteredEvents = props.filteredEvents;
+	let filteredEvents = loadedEvents.filter((event) => {
+		const eventDate = new Date(event.date);
+		return (
+			eventDate.getFullYear() === filteredYear && eventDate.getMonth() === filteredMonth - 1
+		);
+	});
+
 
 	// check if the events are within the range
 	if(!filteredEvents || filteredEvents.length === 0){
@@ -38,45 +64,11 @@ export default function FilteredEventPage(props) {
 		</>
 	}
 
-	const date = new Date(props.date.year, props.date.month - 1)
+	const date = new Date(filteredYear, filteredMonth - 1)
 	return (
 		<>
 			<ResultsTitle date={date}/>
 			<EventList items={filteredEvents}/>
 		</>
 	);
-}
-
-export async function getServerSideProps(context){
-	const { slug } = context.params;
-
-	const filteredMonth = Number(slug[1])
-	const filteredYear = Number(slug[0]);
-
-	if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredYear < 2021 || filteredMonth > 12  ){
-		return {
-			props: {
-				hasError: true
-			},
-			// notFound: true,
-			// redirect: {
-			// 	destination: '/error'
-			// }
-		}
-	}
-	const filteredEvents = await getFilteredEvents({
-		year: filteredYear,
-		month: filteredMonth
-	})
-
-	return {
-		props: {
-			filteredEvents,
-			date: {
-				year: filteredYear,
-				month: filteredMonth
-			}
-		}
-	}
-
 }
