16ae1cb1772eabd221d7a444a67b82a9ab88847b
acharayaP03 <acharyaP03@gmail.com>
2023-04-05 22:00:33 +1000

    add comment api and FE

--- components/inputs/comments.js
+++ components/inputs/comments.js
@@ -1,20 +1,42 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comments';
 import classes from './comments.module.css';
 
+
 function Comments(props) {
     const { eventId } = props;
 
     const [showComments, setShowComments] = useState(false);
+    const [comments, setComments ] = useState([]);
+
+    useEffect( () => {
+        if(showComments){
+            fetch('/api/comments/' + eventId).then( response => response.json()).then(data => setComments(data.comments))
+        }
+    }, [showComments])
 
     function toggleCommentsHandler() {
         setShowComments((prevStatus) => !prevStatus);
     }
 
-    function addCommentHandler(commentData) {
-        // send data to API
+    async function addCommentHandler(commentData) {
+
+        try{
+            const request = await fetch('/api/comments/' + eventId , {
+                method: 'POST',
+                body: JSON.stringify(commentData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+
+            const comment = await request.json();
+            return comment
+        }catch(error){
+            console.error(error)
+        }
     }
 
     return (
@@ -23,7 +45,7 @@ function Comments(props) {
                 {showComments ? 'Hide' : 'Show'} Comments
             </button>
             {showComments && <NewComment onAddComment={addCommentHandler} />}
-            {showComments && <CommentList />}
+            {showComments && <CommentList items={comments}/>}
         </section>
     );
 }
--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,7 +1,6 @@
-import {router} from "next/client";
 
-function handler (req, res) {
-    const eventId = router.query.eventId;
+export default function handler (req, res) {
+    const { eventId } = req.query
 
     if(req.method === 'POST'){
         const { email, name, text } = req.body;
@@ -15,19 +14,15 @@ function handler (req, res) {
             id: new Date().toISOString(),
             email, name, text
         }
-
-        console.log(newComments)
         res.status(201).json({ message: 'Comment created', comment: newComments})
     }
 
     if(req.method === 'GET'){
         const dummy = [
-            { id: 'c1', name: 'Trishten', text: 'My first comment.'},
-            { id: 'c2', name: 'Prabhakar', text: 'My second comment'}
+            { id: 'c1', name: 'Trishten', text: 'Awesome event, I m excited to see how it folds..'},
+            { id: 'c2', name: 'Prabhakar', text: 'Wow, excited to go to this event. Well done!!!'}
         ]
 
         res.status(200).json({ comments: dummy })
     }
 }
-
-export default handler;
