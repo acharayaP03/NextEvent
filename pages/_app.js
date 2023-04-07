c82d018699187cf1e79b067dc1e38a8e3177e911
acharayaP03 <acharyaP03@gmail.com>
2023-04-08 08:59:48 +1000

    create global notification component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Layout from "../components/layouts/layout";
 import Head from "next/head";
+import Notification from "../components/notification/notification";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
               <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
           </Head>
         <Component {...pageProps} />
+        <Notification title="test" message="This is a test" status="error"/>
       </Layout>
   )
 }
