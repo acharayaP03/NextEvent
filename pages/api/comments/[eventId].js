98c3acb8902638b7128f691e476a4e974180c7c7
acharayaP03 <acharyaP03@gmail.com>
2023-04-07 10:32:47 +1000

    get all the comments

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -26,12 +26,10 @@ export default async function handler (req, res) {
     }
 
     if(req.method === 'GET'){
-        const dummy = [
-            { id: 'c1', name: 'Trishten', text: 'Awesome event, I m excited to see how it folds..'},
-            { id: 'c2', name: 'Prabhakar', text: 'Wow, excited to go to this event. Well done!!!'}
-        ]
+        const db = client.db();
 
-        res.status(200).json({ comments: dummy })
+        const documents = await db.collection('comments').find().sort({ _id: -1 }).toArray();
+        res.status(200).json({ comments: documents })
     }
 
     await  client.close();
