e3be50e0cc8634e76204301816be3dcdd09311fc
acharayaP03 <acharyaP03@gmail.com>
2023-04-07 11:07:44 +1000

    error handeling for newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,7 +1,22 @@
 
 import { MongoClient } from "mongodb";
+async function connectDatabase(res) {
+    try{
+        return await MongoClient.connect('mongodb+srv://acharyap03:yA7Z8cyirFCyIuOx@cluster0.8hbzrjl.mongodb.net/events?retryWrites=true&w=majority')
+    }catch (error){
+        res.status(500).json({ message : 'Connection failed...'});
+        return;
+    }
+
+}
+
+async function createSubscription (client, document){
+    const db = client.db();
+    await db.collection('emails').insertOne(document);
+}
 
 export default async function handler(req, res){
+    const client = await connectDatabase(res)
 
     if(req.method === 'POST'){
         const userEmail = req.body.email;
@@ -13,12 +28,14 @@ export default async function handler(req, res){
             return;
         }
 
-       const client =  await  MongoClient.connect(
-            'mongodb+srv://acharyap03:yA7Z8cyirFCyIuOx@cluster0.8hbzrjl.mongodb.net/events?retryWrites=true&w=majority'
-        );
-        const db = client.db();
-        await db.collection('emails').insertOne({ email: userEmail });
+        try{
+            await createSubscription(client, { email: userEmail });
+            await client.close();
 
+        }catch (error){
+            res.status(500).json({ message: 'Email subscription failed.'})
+            return
+        }
         await client.close();
 
         res.status(201).json({ message: 'Successfully Subscribed'})
